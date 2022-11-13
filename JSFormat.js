1a6e662d972c89402cf9025292620c2d2471a54b
Nathan K <nathankim012753@gmail.com>
2022-11-13 12:49:40 -0500

    Test Commits

---
+++ JSFormat.js
@@ -0,0 +1,8 @@
+alert ('Hello World!');
+
+let message = "Hello User!";
+alert(message);
+
+let name = prompt("Actually what is your name?");
+let newMessage = "Greetings, " + name;
+alert(newMessage);
