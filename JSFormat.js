dff949319120874b95e2b0e3feca92594a22bdce
Nathan K <nathankim012753@gmail.com>
2022-11-15 02:46:58 -0500

    HUGE CHANGES

--- JSFormat.js
+++ JSFormat.js
@@ -1,8 +1,44 @@
-alert ('Hello World!');
+function hideFunction(){
+   if (document.querySelector("#SandH").textContent == "Show Password"){
+     document.querySelector("#SandH").textContent = "Hide Password";
+   }
+  else{
+    document.querySelector("#SandH").textContent = "Show Password";
+  }
+  
+  var x = document.querySelector("#Div");
+  if (x.getAttribute("hidden")){
+    x.removeAttribute("hidden");
+  }
+  else{
+    x.setAttribute("hidden", "hidden");
+  }
 
-let message = "Hello User!";
-alert(message);
+  var y = document.querySelector("#TextOne");
+  if (y.getAttribute("hidden")){
+    y.removeAttribute("hidden");
+  }
+  else{
+    y.setAttribute("hidden", "hidden");
+  }
+  
+  var z = document.querySelector("#PWButton");
+  if (z.getAttribute("hidden")){
+    z.removeAttribute("hidden");
+  }
+  else{
+    z.setAttribute("hidden", "hidden");
+  }
+//   if (x.style.display === "none") {
+    // x.style.display = "block";
+//   } else {
+    // x.style.display = "none";
+//   }
 
-let name = prompt("Actually what is your name?");
-let newMessage = "Greetings, " + name;
-alert(newMessage);
+  
+}
+function ifButtonClicked(){
+    document.getElementById("Div").textContent = "Password: " + document.querySelector("#TextOne").value;
+}
+let PWButton = document.getElementById("PWButton");
+PWButton.addEventListener("click", ifButtonClicked );
